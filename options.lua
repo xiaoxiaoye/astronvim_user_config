ee888bb268a503a66657a93004a32cac8e6e34a8
yejiaxin <yejia_xin@outlook.com>
2023-11-06 17:51:23 +0800

    use vscode color theme

--- options.lua
+++ options.lua
@@ -7,7 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
-    background = "light"
+    -- background = "light"
+    background = "dark"
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
