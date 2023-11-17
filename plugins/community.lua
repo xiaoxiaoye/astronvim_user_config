6554a0d4a1269f9e2759a988fbc3545d0b9b7de5
yejiaxin <yejiaxin@yejiaxindebijibendiannao.local>
2023-11-17 10:17:08 +0800

    修改隐藏文件配置

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
 }
