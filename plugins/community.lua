cd3e3cf321765781df2788c9d280bdd2d307fdde
yejiaxin <yejiaxin@yejiaxindebijibendiannao.local>
2023-11-29 12:47:23 +0800

    增加statusline的文件路径和当前所处函数

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  "nvim-tree/nvim-web-devicons",
+  "SmiteshP/nvim-navic",
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.catppuccin" },
