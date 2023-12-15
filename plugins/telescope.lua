228894efefd58105a46f81b3b250cc6ab667cada
yejiaxin <yejia_xin@outlook.com>
2023-12-15 16:36:00 +0800

    LSP 搜索文件展示长度

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -8,14 +8,31 @@ return {
         path_display = {
           shorten = {len = 2, exclude = {1, -1}}
         },
+        wrap_results = false,
+        dynamic_preview_title = true,
         layout_config = {
           horizontal = { prompt_position = "top", preview_width = 0.45 },
           vertical = { mirror = false },
           width = 0.95,
           height = 0.80,
-          preview_cutoff = 100,
+          preview_cutoff = 40,
         }
       },
+      pickers = {
+        -- find_files = {
+        --   hidden = true,
+        --   follow = true,
+        --   previewer = false,
+        -- },
+        -- buffers = {
+        --   sort_lastused = true,
+        --   previewer = false,
+        -- },
+        lsp_references = {
+          -- previewer = false,
+          fname_width = 50,
+        },
+      }
     }
   end,
 }
